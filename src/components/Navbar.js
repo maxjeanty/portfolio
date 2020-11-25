28bf93c4c7d74d7fe6d673e88dc5e99419170a87
maxjeanty <maxjeanty@gmail.com>
2020-11-24 22:00:34 -0600

    pushing from gui desktop

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+function navbar() {
+    return (
+       <>
+       <nav className ="navbar">
+         <div className="navbar-container">
+            <link to = "/"className= ""></link>
+
+         </div>
+
+
+       </nav>
+
+
+
+       </>
+    )
+}
+
+export default navbar
---
+++ src/resumeData.js
@@ -0,0 +1,79 @@
+let resumeData = {
+    "imagebaseurl":"https://scontent.ford4-1.fna.fbcdn.net/v/t1.0-9/581105_365332593512262_468462072_n.jpg?_nc_cat=100&ccb=2&_nc_sid=19026a&_nc_ohc=NVWvy4JszfkAX-3t23v&_nc_ht=scontent.ford4-1.fna&oh=41e5a9c971c8b0dbad667633ab1634e9&oe=5FE50CC7",
+    "name": "Max Jeanty",
+    "role": "Enrolled at Northwestern",
+    "linkedinId":"https://www.linkedin.com/in/maxence-jeanty-9340b390/",
+    
+    "roleDescription":"",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/maxence-jeanty-9340b390//",
+          "className":"fa fahttps://www.linkedin.com/in/maxence-jeanty-9340b390/-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/maxjeanty",
+          "className":"fa fa-github"
+        },
+        
+      ],
+    "aboutme":"Current student at Northwestern.",
+    "address":"Chicago, Delmas Ouest",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"University of the Virgin islands",
+        "specialization":"Social Science",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2013",
+        "Achievements":"Some Achievements"
+      },
+     
+    ],
+    "work":[
+      {
+        "CompanyName":"self Employed",
+        "specialization":"fluent in French, Antillean Creole, English"
+        
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Python"
+      }
+
+
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"under construction",
+       
+      },
+      {
+        "name":"project2",
+        "description":"under construction",
+        
+      }
+    ]
+  }
+  
+  export default resumeData
